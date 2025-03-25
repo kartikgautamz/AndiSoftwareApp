960a40b221439764246a6f832f30d31b027a6685
Kartik Gautam <kartikgautam3377@gmail.com>
2025-03-25 22:41:33 +0530

    Initial Commit

---
+++ AndiSoftwareApp/AndiSoftwareAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndiSoftwareAppApp.swift
+//  AndiSoftwareApp
+//
+//  Created by Kartik Gautam on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AndiSoftwareAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
